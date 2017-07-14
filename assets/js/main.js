e5cde482666f8213df02db202d48df81b59b24c3
camila-frangp <camila.frangp@gmail.com>
2017-07-13 23:17:51 -0400

    EStructura, estilos y funciones de index-contenido.html donde aparece el menú slide y será la base para crear contenido con manejo de DOM de las demás pantallas de la app

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,30 @@
-/* Holiii acá va tu código también */
-//alert("Funciona");
+/*$('.showmenu').on('click', function(e){
+  $('.content').toggleClass('show');
+  $('.ul').toggleClass('ul-color');
+    $('.ul').css('z-index','1000');
+
+
+  e.preventDefault();
+
+});*/
+
+    $(document).ready(function(){												
+       
+       //Navigation Menu Slider
+        $('#nav-expander').on('click',function(e){
+      		e.preventDefault();
+      		$('header').toggleClass('nav-expanded');
+      		$('.pull-left').css("display","none");
+
+      	});
+      	$('#nav-close').on('click',function(e){
+      		e.preventDefault();
+      		$('header').removeClass('nav-expanded');
+      		$('.pull-left').css("display","inline-block");
+      	});
+      	
+      	
+      	
+  
+        	
+      });
