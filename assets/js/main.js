32e196c5e843ca7de3549d2df8c17ae4242ff963
camila-frangp <camila.frangp@gmail.com>
2017-07-14 02:05:02 -0400

    EStructura y estilos de div saldo con sus botones linkeado a él

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,50 @@ $(document).ready(function(){
 						'</div>' +
 					'</div>') ; 
 
+    var preguntas = (	'<div class="container preguntas-c sacar ">' +
+							'<div class="row ">' +
+								'<div class="col-sm-12 col-xs-12 text-center">' + 
+									'<h3>PREGUNTAS<br>FRECUENTES</h3>' + 
+								'</div>' +
+							'</div>' +
+							'<div class="row">' + 
+								'<div class="col-sm-9 col-xs-8 col-xs-offset-2 col-sm-offset-2 preg-frec">' +
+							'</div>' +
+							'</div>' +
+						'</div>');
+
+    var saldo = (	'<div class="container saldo-c sacar ">' +
+						'<div class="row ">' + 
+							'<div class="col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 num-tar">'+
+								'<p>Número de Tarjeta</p>' +
+							'</div>' +
+							'<div class="col-sm-10 col-xs-10  sel-tar">' +
+								'<select class=" form-control elegir-tarjeta" id="select-card">' +
+									'<option>Bip1</option>' +
+									'<option>Bip2</option>' +
+									'<option>Bip3</option>' +
+									'<option>Bip4</option>' +
+									'<option>Bip5</option>' +
+								'</select>' +
+							'</div>' +
+						'</div>' +
+						'<div class="row">' +
+							'<button type="submit" class="btn  btn-saldo" id="ver-total">VER SALDO</button>' +
+						'</div>' +
+					
+						'<div class="row text-center no-ver-saldo">' +
+							'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 saldo-disponible-titulo">' +
+								'<div><p><small>SALDO TOTAL</small></p></div>' +
+							'</div>' +
+						'</div>' +
+						'<div class="row text-center no-ver-saldo">' +
+							'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 saldo-disponible">' +
+								'<div id="saldo-total"><p><big>$1.234</big></p></div>' +
+							'</div>' +
+						'</div>' +
+					'</div>'
+					);
+
 
     $('.btn-inicio').click(function(){
        	window.location.href = "index-contenido.html"
@@ -35,12 +79,28 @@ $(document).ready(function(){
     $('.home').click(function(){
     	$('.sacar').remove();
     	$('#dinamico').append(home);
-    })
+    });
 
     $('.perfil').click(function(){
     	$('.sacar').remove();
     	$('#dinamico').append(perfil);
-    })
+    });
+
+    $('.preguntas').click(function(){
+    	$('.sacar').remove();
+    	$('#dinamico').append(preguntas);
+    });
+
+    $('.saldo').click(function(){
+    	$('.sacar').remove();
+    	$('#dinamico').append(saldo);
+    });
+
+    $('#ver-total').on("click",function(){
+    	$('.no-ver-saldo').css("display","block");
+    });
+
+
 
 
     
