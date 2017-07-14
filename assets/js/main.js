12142b51adcfc813fe83a0a9f884bc5e1d080098
camila-frangp <camila.frangp@gmail.com>
2017-07-14 07:04:17 -0400

    EStructura y estilos de div calcular tarifa con botones respectivos que al clickearos aparece el div correspondiente

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,20 +57,80 @@ $(document).ready(function(){
 						'<div class="row">' +
 							'<button type="submit" class="btn  btn-saldo" id="ver-total">VER SALDO</button>' +
 						'</div>' +
-					
-						'<div class="row text-center no-ver-saldo">' +
-							'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 saldo-disponible-titulo">' +
-								'<div><p><small>SALDO TOTAL</small></p></div>' +
-							'</div>' +
+					'</div>');
+
+    var verSaldo = ('<div class="ver-ver">'+
+    	'<div class="row text-center">' +
+						'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 saldo-disponible-titulo">' +
+							'<div><p><small>SALDO TOTAL</small></p></div>' +
 						'</div>' +
-						'<div class="row text-center no-ver-saldo">' +
-							'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 saldo-disponible">' +
-								'<div id="saldo-total"><p><big>$1.234</big></p></div>' +
-							'</div>' +
+					'</div>' +
+					'<div class="row text-center">' +
+						'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 saldo-disponible">' +
+							'<div id="saldo-total"><p><big>$1.234</big></p></div>' +
 						'</div>' +
-					'</div>'
+					'</div>'+
+				'</div>'
 					);
 
+var calcular = ('<div class="container calcular-c sacar ">'+
+			        '<div class="row">'+
+			            '<div class="col-xs-12">'+
+			                '<div class="col-sm-10 col-xs-10 col-xs-offset-1 col-sm-offset-1 num-tar">'+
+							'<p>Número de Tarjeta</p>'+
+						'</div>'+
+			            ' <div class="col-xs-6 select-cal">'+
+			                '<span>Tarifa</span>'+
+			                '<select class="form-control ">'+
+			                        '<option>-Bip 1-</option>'+
+			                        '<option>Bip 2</option>'+
+			                        '<option>Bip 3</option>'+
+			                        '<option>Bip 4</option>'+
+			                        '<option>Bip 5</option>'+    
+			                    '</select>'+
+			                '</div>'+
+			                '<div class="col-xs-6 select-cal">'+
+			                    '<span>Tarjeta</span>'+
+			                   ' <select class="form-control ">'+
+			                        '<option>-Bip A</option>'+
+			                        '<option>Bip B</option>'+
+			                        '<option>Bip C</option>'+
+			                        '<option>Bip D</option>'+
+			                        '<option>Bip E</option>  '  +
+			                    '</select>'+
+			                '</div>'+
+			                '<div class="row">'+
+								'<button type="submit" class="btn  btn-calcular">CALCULAR</button>'+
+							'</div>'+
+					'</div>'+
+				'</div>');
+
+var carlcularVer = ('<div class="ver-costo">' +
+						'<div class="row text-center">' +
+							'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 cal-disponible-titulo">' +
+								'<div><p><small>COSTO VIAJE</small></p></div>' +
+							'</div>' +
+						'</div>' +
+						'<div class="row text-center">' + 
+							'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 cal-disponible">' +
+								'<div id="cal-tarifa"><p><big>$680</big></p></div>' +
+							'</div>' +
+						'</div>' +
+					'</div>' +
+					'<div class="ver-total">' +
+						'<div class="row text-center">' +
+							'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 cal-disponible-titulo">' +
+								'<div><p><small>SALDO FINAL</small></p></div>' +
+							'</div>' +
+						'</div>' +
+						'<div class="row text-center">' +
+							'<div class="col-sm-6 col-xs-6 col-xs-offset-3 col-sm-offset-3 cal-disponible">' +
+								'<div id="cal-total"><p><big>$1.234</big></p></div>' +
+							'</div>' +
+						'</div>' +
+					'</div>');
+					
+	            
 
     $('.btn-inicio').click(function(){
        	window.location.href = "index-contenido.html"
@@ -94,11 +154,20 @@ $(document).ready(function(){
     $('.saldo').click(function(){
     	$('.sacar').remove();
     	$('#dinamico').append(saldo);
+    	$('#ver-total').click(function(){
+	    	$('.container.saldo-c.sacar').append(verSaldo);
+	    });
     });
 
-    $('#ver-total').on("click",function(){
-    	$('.no-ver-saldo').css("display","block");
+     $('.tarifa').click(function(){
+    	$('.sacar').remove();
+    	$('#dinamico').append(calcular);
+    	$('.btn-calcular').click(function(){
+	    	$('.container.calcular-c.sacar').append(carlcularVer);
+	    });
     });
+
+    
 
 
 
